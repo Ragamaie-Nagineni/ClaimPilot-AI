598a7d6841e59d9e4c62560dce8cb24e594408ca
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-06-04 08:22:57 +0530

    set up backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import {Pool} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+//import authRoutes from "./routes/authRoutes.js"
+import pool from "./db/db.js";
+
+const app=express();
+const port=3000;
+app.use(cors());
+app.use(express.json());
+//app.use("/api/auth",authRoutes);
+
+
+
+app.get("/",(req,res)=>{
+    res.json({message:"hello from backend"});
+})
+
+app.listen((port),()=>{
+    console.log(`app is listening to  port ${port}`);
+})

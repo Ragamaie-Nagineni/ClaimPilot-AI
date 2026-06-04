94c3a61fa770638a01592ecda4bf1c646ab8d43b
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-06-04 22:09:43 +0530

    fixed api export

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-/* import axios from "axios";
+ import axios from "axios";
 
 export const analyzeClaim =
 async (files) => {
@@ -20,11 +20,4 @@ async (files) => {
         );
 
     return response.data;
-}; */
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-export default API;
+}; 

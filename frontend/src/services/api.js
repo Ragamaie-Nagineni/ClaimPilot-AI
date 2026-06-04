fd24f2d16471ac5da20d977b2b223543d5c92acf
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-06-04 22:01:31 +0530

    connect frontend to render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/* import axios from "axios";
 
 export const analyzeClaim =
 async (files) => {
@@ -15,9 +15,16 @@ async (files) => {
 
     const response =
         await axios.post(
-            "http://localhost:3000/api/claims/analyze",
+           "https://claimpilot-ai-04z9.onrender.com/api/claims/analyze",
             formData
         );
 
     return response.data;
-};
+}; */
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default API;

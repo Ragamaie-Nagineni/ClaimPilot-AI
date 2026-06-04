858f97d80d644f6285580c23b35f02ba11d18115
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-06-04 21:45:40 +0530

    fixed render port

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 import express from "express";
 import cors from "cors";
-import {Pool} from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
-//import authRoutes from "./routes/authRoutes.js"
+
 import pool from "./db/db.js";
 import claimRoutes from "./routes/claimroutes.js";
 
+const app = express();
 
-const app=express();
-const port=3000;
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/claims", claimRoutes);
-//app.use("/api/auth",authRoutes);
 
+app.get("/", (req, res) => {
+    res.json({ message: "hello from backend" });
+});
 
-
-app.get("/",(req,res)=>{
-    res.json({message:"hello from backend"});
-})
-
-app.listen((port),()=>{
-    console.log(`app is listening to  port ${port}`);
-})
+app.listen(PORT, () => {
+    console.log(`app is listening to port ${PORT}`);
+});
